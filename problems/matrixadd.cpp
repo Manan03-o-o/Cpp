62d75b2850a3955893389297ae5461775d4dc185
Manan <manannagpal003@gmail.com>
2025-11-26 21:52:25 +0530

    added matrix addition

---
+++ problems/matrixadd.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[2][2], b[2][2], c[2][2];
+
+    cout << "Enter matrix A:\n";
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            cin >> a[i][j];
+
+    cout << "Enter matrix B:\n";
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            cin >> b[i][j];
+
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            c[i][j] = a[i][j] + b[i][j];
+
+    cout << "Result:\n";
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++)
+            cout << c[i][j] << " ";
+        cout << endl;
+    }
+}
