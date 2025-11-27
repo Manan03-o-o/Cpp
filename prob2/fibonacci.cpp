becb4c368ff6bea532cefcfd6e53542a17190222
Manan <manannagpal003@gmail.com>
2025-11-27 21:49:53 +0530

    created fibonacci num

---
+++ prob2/fibonacci.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a = 0, b = 1;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
