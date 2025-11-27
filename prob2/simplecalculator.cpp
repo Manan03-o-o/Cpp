42dbc36422fff470f392d8ce69908bf806d526df
Manan <manannagpal003@gmail.com>
2025-11-27 21:46:28 +0530

    created simple calculator

---
+++ prob2/simplecalculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    char op;
+
+    cin >> a >> b >> op;
+
+    switch(op) {
+        case '+': cout << a + b; break;
+        case '-': cout << a - b; break;
+        case '*': cout << a * b; break;
+        case '/': cout << a / b; break;
+        default: cout << "Invalid operator";
+    }
+
+    return 0;
+}
