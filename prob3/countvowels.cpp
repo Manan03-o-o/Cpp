ed04c26e7449d0b5d7395bd7b684ef6c007cfcb2
Manan <manannagpal003@gmail.com>
2025-11-29 19:58:17 +0530

    created vowels counter

---
+++ prob3/countvowels.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    int count = 0;
+    for (char c : s) {
+        c = tolower(c);
+        if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+            count++;
+    }
+
+    cout << "Vowels = " << count;
+    return 0;
+}
