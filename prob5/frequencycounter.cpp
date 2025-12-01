58aed29417801dd9ef51cd7918bf6f0a1ff8f666
Manan <manannagpal003@gmail.com>
2025-12-01 21:26:58 +0530

    created frequency counter

---
+++ prob5/frequencycounter.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    map<char, int> freq;
+
+    for (char c : s)
+        freq[c]++;
+
+    for (auto p : freq)
+        cout << p.first << " : " << p.second << endl;
+
+    return 0;
+}
