2a8abf2ee3e58b64cffa7975f21a92b66f5b1523
Manan <manannagpal003@gmail.com>
2025-12-04 21:18:22 +0530

    count no even odd

---
+++ prob8/count.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    int even=0, odd=0, pos=0, neg=0;
+
+    for (int x : arr) {
+        if (x % 2 == 0) even++;
+        else odd++;
+
+        if (x > 0) pos++;
+        else if (x < 0) neg++;
+    }
+
+    cout << "Even = " << even << endl;
+    cout << "Odd = " << odd << endl;
+    cout << "Positive = " << pos << endl;
+    cout << "Negative = " << neg << endl;
+
+    return 0;
+}
